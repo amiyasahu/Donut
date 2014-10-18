361eb37a0fdc609234e1b406347836a02982364b
Amiya <amiya053@gmail.com>
2014-10-18 15:48:22 +0530

    small change

--- qa-donut-layer.php
+++ qa-donut-layer.php
@@ -516,7 +516,7 @@
 			/*Please do not remove this as you are using this for free . I will appriciate if you keep this on your site */
 			$this->output(
 				'<div class="qa-attribution">',
-				'&nbsp;| Donut Theme by <a href="http://www.amiyasahu.com">Amiya Sahu</a>',
+				'&nbsp;| Donut Theme by <a href="http://amiyasahu.com">Amiya Sahu</a>',
 				'</div>'
 			);
 
