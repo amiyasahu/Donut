1df2a6e00ada8fe6a7d39318a4727e845e29223f
Amiya <amiya053@gmail.com>
2014-10-26 16:28:48 +0530

    navigations improvements

--- utils/qa-donut-utils.php
+++ utils/qa-donut-utils.php
@@ -37,15 +37,15 @@ if (!function_exists('donut_get_sub_navigation')) {
 		switch ($page_type) {
 			case 'questions':
 				$sort = qa_get('sort');
-				$sub_nav=qa_qs_sub_navigation($sort, array());
+				$sub_nav=donut_qs_sub_navigation($sort, array());
 				break;
 			case 'unanswered':
 				$categoryslugs=qa_request_parts(1);
 				$by=qa_get('by');
-				$sub_nav=qa_unanswered_sub_navigation($by, $categoryslugs);
+				$sub_nav=donut_unanswered_sub_navigation($by, $categoryslugs);
 				break;
 			case 'users':
-				$sub_nav=qa_users_sub_navigation();
+				$sub_nav=donut_users_sub_navigation();
 				break;
 			case 'admin':
 				$sub_nav=qa_admin_sub_navigation();
@@ -129,3 +129,138 @@ if (!function_exists('donut_remove_brackets')) {
 	}
 }
 
+if (!function_exists('donut_qs_sub_navigation')) {
+	function donut_qs_sub_navigation($sort, $categoryslugs)
+	{
+		$request='questions';
+
+		if (isset($categoryslugs))
+			foreach ($categoryslugs as $slug)
+				$request.='/'.$slug;
+
+		$navigation=array(
+			'recent' => array(
+				'label' => qa_lang('main/nav_most_recent'),
+				'url' => qa_path_html($request),
+			),
+			
+			'hot' => array(
+				'label' => qa_lang('main/nav_hot'),
+				'url' => qa_path_html($request, array('sort' => 'hot')),
+			),
+			
+			'votes' => array(
+				'label' => qa_lang('main/nav_most_votes'),
+				'url' => qa_path_html($request, array('sort' => 'votes')),
+			),
+
+			'answers' => array(
+				'label' => qa_lang('main/nav_most_answers'),
+				'url' => qa_path_html($request, array('sort' => 'answers')),
+			),
+
+			'views' => array(
+				'label' => qa_lang('main/nav_most_views'),
+				'url' => qa_path_html($request, array('sort' => 'views')),
+			),
+		);
+		
+		if (isset($navigation[$sort])){
+			$navigation[$sort]['selected']=true;
+		}else{
+			$request_parts = qa_request_parts();
+			if ( !empty($request_parts) && $request_parts[0] == 'questions' ) {
+				$navigation['recent']['selected']=true;
+			}
+		}
+		
+		if (!qa_opt('do_count_q_views'))
+			unset($navigation['views']);
+		
+		return $navigation;
+	}
+}
+
+if (!function_exists('donut_unanswered_sub_navigation')) {
+	function donut_unanswered_sub_navigation($by, $categoryslugs)
+/*
+	Return the sub navigation structure common to unanswered pages
+*/
+	{
+		$request='unanswered';
+
+		if (isset($categoryslugs))
+			foreach ($categoryslugs as $slug)
+				$request.='/'.$slug;
+		
+		$navigation=array(
+			'by-answers' => array(
+				'label' => qa_lang('main/nav_no_answer'),
+				'url' => qa_path_html($request),
+			),
+			
+			'by-selected' => array(
+				'label' => qa_lang('main/nav_no_selected_answer'),
+				'url' => qa_path_html($request, array('by' => 'selected')),
+			),
+			
+			'by-upvotes' => array(
+				'label' => qa_lang('main/nav_no_upvoted_answer'),
+				'url' => qa_path_html($request, array('by' => 'upvotes')),
+			),
+		);
+		
+		if (isset($navigation['by-'.$by])){
+			$navigation['by-'.$by]['selected']=true;
+		}else{
+			$request_parts = qa_request_parts();
+			if ( !empty($request_parts) && $request_parts[0] == 'unanswered' ) {
+				$navigation['by-answers']['selected']=true;
+			}
+		}
+			
+		if (!qa_opt('voting_on_as'))
+			unset($navigation['by-upvotes']);
+
+		return $navigation;
+	}
+}
+
+if (!function_exists('donut_users_sub_navigation')) {
+	function donut_users_sub_navigation()
+	{
+		if ((!QA_FINAL_EXTERNAL_USERS) && (qa_get_logged_in_level()>=QA_USER_LEVEL_MODERATOR)) {
+			$navigation = array(
+				'users$' => array(
+					'url' => qa_path_html('users'),
+					'label' => qa_lang_html('main/highest_users'),
+				),
+	
+				'users/special' => array(
+					'label' => qa_lang('users/special_users'),
+					'url' => qa_path_html('users/special'),
+				),
+	
+				'users/blocked' => array(
+					'label' => qa_lang('users/blocked_users'),
+					'url' => qa_path_html('users/blocked'),
+				),
+			);
+			
+			$request_parts = qa_request_parts();
+			if ( !empty($request_parts) && $request_parts[0] == 'users') {
+				if (count($request_parts) == 1) {
+					$navigation['users$']['selected']=true;	
+				}else if (count($request_parts) > 1 && $request_parts[1] == 'special') {
+					$navigation['users/special']['selected']=true;	
+				}else if (count($request_parts) > 1 && $request_parts[1] == 'blocked') {
+					$navigation['users/blocked']['selected']=true;	
+				}
+			}
+
+			return $navigation;
+
+		} else
+			return null;
+	}
+}
