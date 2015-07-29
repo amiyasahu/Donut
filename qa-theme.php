780eeae0e7b36e6c17624e89057f908bebdd727b
Amiya Sahu <developer.amiya@outlook.com>
2015-07-29 23:16:04 +0530

    refractor

--- qa-theme.php
+++ qa-theme.php
@@ -1,64 +1,64 @@
-<?php 
-if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
-	header('Location: ../../');
-	exit;
-}
+<?php
+    if ( !defined( 'QA_VERSION' ) ) { // don't allow this page to be requested directly from browser
+        header( 'Location: ../../' );
+        exit;
+    }
 
-/**
- * Defines the base directory of the theme 
- */
-if (!defined('DONUT_THEME_BASE_DIR')) {
-	define('DONUT_THEME_BASE_DIR', dirname(__FILE__));
-}
-/**
- * define the directory name of the theme directory 
- */
-if (!defined('DONUT_THEME_BASE_DIR_NAME')) {
-	define('DONUT_THEME_BASE_DIR_NAME', basename(dirname(__FILE__)));
-}
+    /**
+     * Defines the base directory of the theme
+     */
+    if ( !defined( 'DONUT_THEME_BASE_DIR' ) ) {
+        define( 'DONUT_THEME_BASE_DIR', dirname( __FILE__ ) );
+    }
+    /**
+     * define the directory name of the theme directory
+     */
+    if ( !defined( 'DONUT_THEME_BASE_DIR_NAME' ) ) {
+        define( 'DONUT_THEME_BASE_DIR_NAME', basename( dirname( __FILE__ ) ) );
+    }
 
-/**
- * define the theme root URL
- */
-if (!defined('DONUT_THEME_ROOT_URL')) {
-	define('DONUT_THEME_ROOT_URL', qa_opt('site_url').'qa-theme/'.DONUT_THEME_BASE_DIR_NAME);
-}
+    /**
+     * define the theme root URL
+     */
+    if ( !defined( 'DONUT_THEME_ROOT_URL' ) ) {
+        define( 'DONUT_THEME_ROOT_URL', qa_opt( 'site_url' ) . 'qa-theme/' . DONUT_THEME_BASE_DIR_NAME );
+    }
 
-/**
- * define the version of the theme that is installed 
- */
-if (!defined('DONUT_THEME_VERSION')) {
-	define('DONUT_THEME_VERSION', 1.0 );
-}
+    /**
+     * define the version of the theme that is installed
+     */
+    if ( !defined( 'DONUT_THEME_VERSION' ) ) {
+        define( 'DONUT_THEME_VERSION', 1.0 );
+    }
 
-/**
- * Please DO NOT Change the version Unique id . 
- * This will increase in each version , that helps you in adding new theme defaults 
- */
-if (!defined('DONUT_THEME_VERSION_UID')) {
-	define('DONUT_THEME_VERSION_UID', 1 );
-}
+    /**
+     * Please DO NOT Change the version Unique id .
+     * This will increase in each version , that helps you in adding new theme defaults
+     */
+    if ( !defined( 'DONUT_THEME_VERSION_UID' ) ) {
+        define( 'DONUT_THEME_VERSION_UID', 1 );
+    }
 
-/**
- * If this option is set to true , the theme will use advanced setups like CDN , 
- * caching to enhance the performance . 
- * If you using this on your local machine with out a internet connection then set it to false 
- */
-if (!defined('DONUT_ACTIVATE_PROD_MODE')) {
-	define('DONUT_ACTIVATE_PROD_MODE', FALSE );
-}
+    /**
+     * If this option is set to true , the theme will use advanced setups like CDN ,
+     * caching to enhance the performance .
+     * If you using this on your local machine with out a internet connection then set it to false
+     */
+    if ( !defined( 'DONUT_ACTIVATE_PROD_MODE' ) ) {
+        define( 'DONUT_ACTIVATE_PROD_MODE', false );
+    }
 
-if (!defined('DONUT_LANG_RTL')) {
-	define('DONUT_LANG_RTL', FALSE );
-}
-/**
- * include the required files for the theme 
- */
-require_once DONUT_THEME_BASE_DIR.'/utils/qa-donut-utils.php';
-require_once DONUT_THEME_BASE_DIR.'/utils/donut-options.php';
-require_once DONUT_THEME_BASE_DIR.'/qa-donut-layer.php';
+    if ( !defined( 'DONUT_LANG_RTL' ) ) {
+        define( 'DONUT_LANG_RTL', false );
+    }
+    /**
+     * include the required files for the theme
+     */
+    require_once DONUT_THEME_BASE_DIR . '/utils/qa-donut-utils.php';
+    require_once DONUT_THEME_BASE_DIR . '/utils/donut-options.php';
+    require_once DONUT_THEME_BASE_DIR . '/qa-donut-layer.php';
 
-/**
- * Declaring global variables for caching the userdata 
- */
-$donut_userid_and_levels=array();
+    /**
+     * Declaring global variables for caching the userdata
+     */
+    $donut_userid_and_levels = array();
--- utils/donut-options.php
+++ utils/donut-options.php
@@ -1,21 +1,21 @@
-<?php 
-if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
-	header('Location: ../../');
-	exit;
-}
-/**
- * This file will contain all the option names we are going to use in out theme 
- */
+<?php
+    if ( !defined( 'QA_VERSION' ) ) { // don't allow this page to be requested directly from browser
+        header( 'Location: ../../' );
+        exit;
+    }
+    /**
+     * This file will contain all the option names we are going to use in out theme
+     */
 
-if (!class_exists('donut_opt')) {
-	class donut_opt
-	{	
-		const THEME_VERSION           = 'donut_theme_ver'	;
-		const INSTALLED_THEME_VERSION = 'donut_theme_ver_instaled'	;
-		const CDN_ENABLED             = 'donut_cdn_active'	;
-		const BS_CSS_CDN              = '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'	;
-		const BS_THEME_CSS_CDN        = '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css' ;
-		const FA_CDN                  = '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css' ;
-		const BS_JS_CDN               = '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js'	;
-	}
-}
+    if ( !class_exists( 'donut_opt' ) ) {
+        class donut_opt
+        {
+            const THEME_VERSION = 'donut_theme_ver';
+            const INSTALLED_THEME_VERSION = 'donut_theme_ver_instaled';
+            const CDN_ENABLED = 'donut_cdn_active';
+            const BS_CSS_CDN = '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css';
+            const BS_THEME_CSS_CDN = '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css';
+            const FA_CDN = '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css';
+            const BS_JS_CDN = '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js';
+        }
+    }
