eb016077892625adfac02e02ebc4ca1c133477fd
Amiya Sahu <developer.amiya@outlook.com>
2015-08-02 15:52:56 +0530

    updated for errors when ajax response

--- qa-donut-layer.php
+++ qa-donut-layer.php
@@ -900,7 +900,7 @@
         {
             if (strlen($error)) {
                 $this->output(
-                    '<div class="qa-error alert alert-dismissible" role="alert">',
+                    '<div class="donut-error alert alert-dismissible" role="alert">',
                     $error,
                     '<button class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' ,
                 '</div>'
