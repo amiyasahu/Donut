eba5090559c846fc1e933367a86c69c4f8a9222d
Amiya Sahu <developer.amiya@outlook.com>
2015-08-13 22:41:19 +0530

    added attribution

--- templates/site-header.php
+++ templates/site-header.php
@@ -42,9 +42,8 @@
                     </div>
                 </div>
             </div>
-            <div class="row search-wrapper">
-                <div
-                    class="search-bar col-lg-4 col-lg-push-4 col-md-6 col-md-push-3 col-sm-8 col-sm-push-2 col-xs-10 col-xs-push-1">
+            <div class="search-wrapper">
+                <div class="search-bar col-lg-4 col-lg-push-4 col-md-6 col-md-push-3 col-sm-8 col-sm-push-2 col-xs-10 col-xs-push-1">
                     <form class="form-inline" method="post" action="<?php echo qa_path_html('ask'); ?>">
                         <div class="form-group form-group-lg">
                             <input type="text" class="form-control input-lg ask-field" id="ask"
@@ -54,6 +53,8 @@
                         <input type="hidden" name="doask1" value="1">
                     </form>
                 </div>
+                <!--IF you are using the default vector , you should not remove the attribution for freepik.com-->
+                <div class="col-lg-12 visible-lg text-right small">vector designed by Freepik.com</div>
             </div>
         </div>
     </div>
