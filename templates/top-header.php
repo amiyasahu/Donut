83994105a7c212a0f851c7688f1f7f759f86c060
Amiya Sahu <developer.amiya@outlook.com>
2015-08-14 23:56:19 +0530

    fixed #22 for older PHP versions

--- templates/top-header.php
+++ templates/top-header.php
@@ -1,23 +1,29 @@
+<?php
+    $header_left_text = donut_opt( 'top_bar_left_text' );
+    $header_right_text = donut_opt( 'top_bar_right_text' );
+    $social_links = donut_opt( 'social_links' );
+?>
 <header id="top-header">
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
                 <div class="left-part pull-left">
-                    <?php if ( !empty( $header_left_text = donut_opt( 'top_bar_left_text' ) ) ): ?>
+                    <?php if ( !empty( $header_left_text ) ): ?>
                         <?php echo $header_left_text ?>
                     <?php endif ?>
                 </div>
                 <div class="right-part pull-right hidden-xs">
-                    <?php if ( !empty( $header_right_text = donut_opt( 'top_bar_right_text' ) ) ): ?>
+
+                    <?php if ( !empty( $header_right_text ) ): ?>
                         <?php echo $header_right_text ?>
                     <?php endif ?>
-                    <?php if (donut_opt('top_bar_add_social_links')): ?>
+                    <?php if ( donut_opt( 'top_bar_add_social_links' ) ): ?>
                         <div class="top-html-block">
                             <div class="social-links">
-                                <?php foreach ($social_links = donut_opt('social_links') as $key => $value): ?>
-                                    <?php 
-                                    if (!empty($value)){
-                                        echo donut_get_social_link($value , true);
+                                <?php foreach ( $social_links as $key => $value ): ?>
+                                    <?php
+                                    if ( !empty( $value ) ) {
+                                        echo donut_get_social_link( $value, true );
                                     }
                                     ?>
                                 <?php endforeach ?>
