0ed1269e0e639449b42bf22194d029c0e3f7d6fd
Amiya Sahu <amiya053@gmail.com>
2020-03-08 18:35:38 -0400

    Remove un-necessary option keys

--- qa-theme/Donut-theme/utils/donut-options.php
+++ qa-theme/Donut-theme/utils/donut-options.php
@@ -11,11 +11,7 @@
     if ( !class_exists( 'Donut_Option_Keys' ) ) {
         class Donut_Option_Keys
         {
-            const THEME_VERSION = 'donut_theme_ver';
-            const INSTALLED_THEME_VERSION = 'donut_theme_ver_instaled';
-            const CDN_ENABLED = 'donut_cdn_active';
             const BS_CSS_CDN = '//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css';
-            const BS_THEME_CSS_CDN = '//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css';
             const FA_CDN = '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css';
             const BS_JS_CDN = '//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js';
         }
